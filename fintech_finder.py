4fae14fb1f0f1f88736e0d49c6c1e6282190d6c6
Ana <anafilipamartelo@gmail.com>
2023-09-16 21:31:50 -0400

    Write the equation that calculates the candidate’s wage. Write the wage variable to the Streamlit sidebar. Call the send_transaction function. Save the transaction hash that the send_transaction function returns as a variable named transaction_hash, and have it display on the application’s web interface.

--- fintech_finder.py
+++ fintech_finder.py
@@ -167,17 +167,16 @@ st.sidebar.write(candidate)
 # Identify the FinTech Finder candidate's hourly rate
 hourly_rate = candidate_database[person][3]
 
-# Write the inTech Finder candidate's hourly rate to the sidebar
+# Write the FinTech Finder candidate's hourly rate to the sidebar
 st.sidebar.write(hourly_rate)
 
 # Identify the FinTech Finder candidate's Ethereum Address
 candidate_address = candidate_database[person][1]
 
-# Write the inTech Finder candidate's Ethereum Address to the sidebar
+# Write the FinTech Finder candidate's Ethereum Address to the sidebar
 st.sidebar.write(candidate_address)
 
-# Write the Fintech Finder candidate's name to the sidebar
-
+# Write the Fintech Finder candidate's total wage in ether to the sidebar
 st.sidebar.markdown("## Total Wage in Ether")
 
 ################################################################################
@@ -237,11 +236,11 @@ st.sidebar.markdown("## Total Wage in Ether")
 # Calculate total `wage` for the candidate by multiplying the candidate’s hourly
 # rate from the candidate database (`candidate_database[person][3]`) by the
 # value of the `hours` variable
-# YOUR CODE HERE
+wage = hourly_rate * hours
 
 # @TODO
 # Write the `wage` calculation to the Streamlit sidebar
-# YOUR CODE HERE
+st.sidebar.write(wage)
 
 ##########################################
 # Step 2 - Part 2:
@@ -268,7 +267,7 @@ if st.sidebar.button("Send Transaction"):
     # Call the `send_transaction` function and pass it 3 parameters:
     # Your `account`, the `candidate_address`, and the `wage` as parameters
     # Save the returned transaction hash as a variable named `transaction_hash`
-    # YOUR CODE HERE
+    transaction_hash = send_transaction(w3, account, candidate_address, wage)
 
     # Markdown for the transaction hash
     st.sidebar.markdown("#### Validated Transaction Hash")
